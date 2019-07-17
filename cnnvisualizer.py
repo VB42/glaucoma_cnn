2e85977105a279838bf14b034bf3633149f0b2ed
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-07-17 20:44:14 +1000

    [*.py] Check pixel ordering; Property path handling with `os.path`; PEP8 & autoformat; better, more consistent file handling; Typo fixes; Upgrade dependencies; [.editorconfig,requirements.txt] Init; [README.md] Updated with instructions for installing from 'requirements.txt'

--- cnnvisualizer.py
+++ cnnvisualizer.py
@@ -1,10 +1,11 @@
-import numpy as np
-from keras.models import Sequential, Model
-from keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, Activation
-from keras import optimizers
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import os
-import csv
+
+from keras import activations
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from keras.models import Sequential
+from keras.preprocessing.image import img_to_array, load_img
+from matplotlib import pyplot as plt
+from vis.visualization import visualize_saliency, visualize_activation, visualize_cam
 
 # import pydot
 #
@@ -12,12 +13,8 @@ import csv
 #
 # import tensorflow as tf
 #
-from time import time, sleep
 #
 # from keras.callbacks import TensorBoard
-
-from keras import activations
-from matplotlib import pyplot as plt
 #
 #
 # from guided_backprop import GuidedBackprop
@@ -28,40 +25,32 @@ from matplotlib import pyplot as plt
 # from dotenv import load_dotenv
 
 
-from vis.utils import utils
-from vis.visualization import visualize_saliency, visualize_activation, visualize_cam
-
-
 width = 224
 height = 224
 
 model = Sequential()
-model.add(Conv2D(64, (4,4), input_shape=(width,height,3), activation='relu'))
+model.add(Conv2D(64, (4, 4), input_shape=(width, height, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(16, (2,2), activation='relu'))
+model.add(Conv2D(16, (2, 2), activation='relu'))
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
 model.add(Dense(128, activation='relu'))
 model.add(Dense(2, activation='softmax'))
 
-model.load_weights("eighth_try.h5")
+model.load_weights('eighth_try.h5')
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adadelta',
               metrics=['accuracy'])
 
 
-
-
-
 def save_img(path, savepath, origimg, typeimg, layeridx):
-
-    img = load_img(path, target_size=(224,224))
-    x = img_to_array(img) #numpy array
-    x = x.reshape(x.shape) #adds on dimension for keras
+    img = load_img(path, target_size=(224, 224))
+    x = img_to_array(img)  # numpy array
+    x = x.reshape(x.shape)  # adds on dimension for keras
 
     model.layers[layeridx].activation = activations.linear
     if typeimg == 'activation':
@@ -79,7 +68,7 @@ def save_img(path, savepath, origimg, typeimg, layeridx):
     if not os.path.exists('image-' + savepath):
         os.makedirs('image-' + savepath)
 
-    combined = str(savepath) + '/' + str(origimg)
+    combined = os.path.join(savepath, origimg)
     plt.imshow(img)
     plt.savefig('layer-' + combined, dpi=600)
     # plt.imshow(x)
@@ -89,17 +78,25 @@ def save_img(path, savepath, origimg, typeimg, layeridx):
 types = ['saliency', 'cam']
 glauc_imgs = ['G-1-L.jpg', 'G-2-R.jpg']
 health_imgs = ['N-1-L.jpg', 'N-2-R.jpg']
-for layeridx in [2,3,4,5,6]:
-    print("LAYER: " + str(layeridx))
+for layeridx in [2, 3, 4, 5, 6]:
+    print('LAYER:', layeridx)
     for typeimg in types:
-        print("TYPE: " + str(typeimg))
-        for imgidx in range(0,2):
-            save_img("rim-flow-datav2/train/glaucoma/" + glauc_imgs[imgidx], 'genimages/' + 'layer_' + str(layeridx) + '/' + typeimg + '/glaucoma', glauc_imgs[imgidx], typeimg, layeridx)
-            save_img("rim-flow-datav2/train/healthy/" + health_imgs[imgidx], 'genimages/' + 'layer_' + str(layeridx) + '/' + typeimg + '/healthy', health_imgs[imgidx], typeimg, layeridx)
+        print('TYPE:', typeimg)
+        for imgidx in range(0, 2):
+            save_img(os.path.join('rim-flow-datav2', 'train', 'glaucoma', glauc_imgs[imgidx]),
+                     os.path.join('genimages' + 'layer_' + str(layeridx), typeimg, 'glaucoma'),
+                     glauc_imgs[imgidx],
+                     typeimg,
+                     layeridx)
+            save_img(os.path.join('rim-flow-datav2', 'train', 'healthy', health_imgs[imgidx]),
+                     os.path.join('genimages' + 'layer_' + str(layeridx), typeimg, 'healthy'),
+                     health_imgs[imgidx],
+                     typeimg,
+                     layeridx)
 
-input_img = load_img("rim-flow-data/train/glaucoma/G-1-L.jpg")
-input_img = img_to_array(input_img) #numpy array
-input_img = input_img.reshape((1,) + input_img.shape) #adds on dimension for keras
+input_img = load_img(os.path.join('rim-flow-data', 'train', 'glaucoma', 'G-1-L.jpg'))
+input_img = img_to_array(input_img)  # numpy array
+input_img = input_img.reshape((1,) + input_img.shape)  # adds on dimension for keras
 
 #
 # layer_dict = dict([(layer.name, layer) for layer in model.layers])
@@ -114,7 +111,7 @@ input_img = input_img.reshape((1,) + input_img.shape) #adds on dimension for ker
 # # compute the gradient of the input picture wrt this loss
 # grads = K.gradients(loss, input_img)[0]
 #
-# print("HIIIIII\n\n\\n\n\n\n\\n\n\n\n")
+# print('HIIIIII\n\n\\n\n\n\n\\n\n\n\n')
 # print(loss)
 #
 # # normalization trick: we normalize the gradient
@@ -153,10 +150,9 @@ input_img = input_img.reshape((1,) + input_img.shape) #adds on dimension for ker
 # imsave('%s_filter_%d.png' % (layer_name, filter_index), img)
 
 
-
 #
-# load_dotenv(".config.env")
-# dst = os.getenv("FORMATTED_DATA_PATH")
+# load_dotenv('.config.env')
+# dst = os.getenv('FORMATTED_DATA_PATH')
 #
 # guided_bprop = GuidedBackprop(model)
 #
--- format_into_folder.py
+++ format_into_folder.py
@@ -1,46 +1,53 @@
-#Run only once to sort out data
-#Glauc Test Data images = ['G-39-L.jpg', 'S-29-R.jpg', 'G-33-R.jpg', 'S-16-R.jpg', 'G-24-L.jpg', 'S-25-L.jpg', 'S-31-L.jpg', 'S-5-L.jpg', 'S-18-L.jpg', 'G-13-R.jpg', 'G-18-R.jpg', 'G-35-R.jpg', 'G-7-L.jpg', 'S-6-R.jpg']
-#Health Test Data images = ['N-47-L.jpg', 'N-70-R.jpg', 'N-58-R.jpg', 'N-11-L.jpg', 'N-22-R.jpg', 'N-69-L.jpg', 'N-13-L.jpg', 'N-81-L.jpg', 'N-6-R.jpg', 'N-21-L.jpg', 'N-18-R.jpg', 'N-44-R.jpg', 'N-3-L.jpg', 'N-90-R.jpg', 'N-23-L.jpg', 'N-55-L.jpg', 'N-32-R.jpg']
-import os, random
-from shutil import copy
-from dotenv import load_dotenv
-from PIL import Image
-load_dotenv(".config.env")
-glauc_path = os.getenv("GLAUC_PATH")
-health_path = os.getenv("HEALTH_PATH")
+# Run only once to sort out data
+# Glauc Test Data images = ['G-39-L.jpg', 'S-29-R.jpg', 'G-33-R.jpg', 'S-16-R.jpg', 'G-24-L.jpg', 'S-25-L.jpg',
+#                           'S-31-L.jpg', 'S-5-L.jpg', 'S-18-L.jpg', 'G-13-R.jpg', 'G-18-R.jpg', 'G-35-R.jpg',
+#                           'G-7-L.jpg', 'S-6-R.jpg']
+# Health Test Data images = ['N-47-L.jpg', 'N-70-R.jpg', 'N-58-R.jpg', 'N-11-L.jpg', 'N-22-R.jpg', 'N-69-L.jpg',
+#                            'N-13-L.jpg', 'N-81-L.jpg', 'N-6-R.jpg', 'N-21-L.jpg', 'N-18-R.jpg', 'N-44-R.jpg',
+#                            'N-3-L.jpg', 'N-90-R.jpg', 'N-23-L.jpg', 'N-55-L.jpg', 'N-32-R.jpg']
+import os
+import random
 
-dst = os.getenv("FORMATTED_DATA_PATH")
+from PIL import Image
+from dotenv import load_dotenv
+
+load_dotenv('.config.env')
+glauc_path = os.getenv('GLAUC_PATH')
+health_path = os.getenv('HEALTH_PATH')
+
+dst = os.getenv('FORMATTED_DATA_PATH')
 
 glauc_dir = os.listdir(glauc_path)
 health_dir = os.listdir(health_path)
 
-
 glauc_train = random.sample(glauc_dir, len(glauc_dir))
 health_train = random.sample(health_dir, len(health_dir))
 
-glauc_test = random.sample(glauc_dir, int(len(glauc_dir)/5))
-health_test = random.sample(health_dir, int(len(health_dir)/5))
+glauc_test = random.sample(glauc_dir, len(glauc_dir) // 5)
+health_test = random.sample(health_dir, len(health_dir) // 5)
 
 glauc_train = list(set(glauc_train).difference(set(glauc_test)))
 health_train = list(set(health_train).difference(set(health_test)))
 
+
 def crop(image_path, loc):
     image_obj = Image.open(image_path)
-    cropped_image = image_obj.crop((0,0,image_obj.size[0]/2,image_obj.size[1]))
+    cropped_image = image_obj.crop((0, 0, image_obj.size[0] / 2, image_obj.size[1]))
     cropped_image.save(loc)
 
+
 for x in glauc_train:
-    crop(glauc_path + '/' + x, dst + '/train/glaucoma/' + x)
-    #copy(glauc_path + '/' + x, dst + '/train/glaucoma/')
+    crop(os.path.join(glauc_path, x), os.path.join(dst, 'train', 'glaucoma', x))
+    # copy(glauc_path + '/' + x, dst + '/train/glaucoma/')
 
 for x in glauc_test:
-    crop(glauc_path + '/' + x, dst + '/validation/glaucoma/' + x)
-    #copy(glauc_path + '/' + x, dst + '/validation/glaucoma')
+    crop(os.path.join(glauc_path, x), os.path.join(dst, 'validation', 'glaucoma', x))
+    # copy(glauc_path + '/' + x, dst + '/validation/glaucoma')
 
 for x in health_train:
-    crop(health_path + '/' + x, dst + '/train/healthy/' + x)
-    #copy(health_path + '/' + x, dst + '/train/healthy')
+    crop(os.path.join(health_path, x), os.path.join(dst, 'train', 'healthy', x))
+    # copy(health_path + '/' + x, dst + '/train/healthy')
 
 for x in health_test:
-    crop(health_path + '/' + x, dst + '/validation/healthy/' + x)
-    #copy(health_path + '/' + x, dst + '/validation/healthy')
+    crop(os.path.join(health_path, x), os.path.join(dst, 'validation', 'healthy', x))
+    # copy(health_path + '/' + x, dst + '/validation/healthy')
